b11c01f25573ef5b951d03da3137d5fb76d00237
Hnin Phyu Phyu Aung <hninphyuphyuaung16@gmail.com>
2023-12-11 00:56:15 +0630

    [Complete] Mvc (dark mode)

---
+++ HPPADotNetCore.Mvc/Controllers/FamilyController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HPPADotNetCore.MvcApp.Controllers
+{
+    public class FamilyController : Controller
+    {
+        [ActionName("Index")]
+        public IActionResult FamilyIndex()
+        {
+            return View("FamilyIndex");
+        }
+    }
+}
---
+++ HPPADotNetCore.Mvc/EFDbContext/AppDbContext.cs
@@ -0,0 +1,21 @@
+﻿using HPPADotNetCore.MvcApp.Models;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HPPADotNetCore.MvcApp.EFDbContext
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<FamilyDataModel> families { get; set; }
+    }
+}
