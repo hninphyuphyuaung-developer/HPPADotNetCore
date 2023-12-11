541fff0567aeab4a8aac41c8fe6a9d66e4039f46
Hnin Phyu Phyu Aung <hninphyuphyuaung16@gmail.com>
2023-12-11 20:57:37 +0630

    [Complete] Mvc Read,Create

--- HPPADotNetCore.Mvc/Controllers/FamilyController.cs
+++ HPPADotNetCore.Mvc/Controllers/FamilyController.cs
@@ -1,13 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HPPADotNetCore.MvcApp.EFDbContext;
+using HPPADotNetCore.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HPPADotNetCore.MvcApp.Controllers
 {
     public class FamilyController : Controller
     {
+        private readonly AppDbContext _context;
+
+        public FamilyController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        //Get / List
         [ActionName("Index")]
         public IActionResult FamilyIndex()
         {
-            return View("FamilyIndex");
+            List<FamilyDataModel> lst = _context.families.ToList();
+            return View("FamilyIndex",lst);
+        }
+
+        [ActionName("Create")]
+        public IActionResult FamilyCreate() 
+        {
+            return View("FamilyCreate");
+        }
+
+        [HttpPost]
+        [ActionName("Save")]
+        public async Task<IActionResult> FamilySave(FamilyDataModel reqModel) 
+        {
+            await _context.families.AddAsync(reqModel);
+            var result = await _context.SaveChangesAsync();
+            string message = result > 0 ? "Saving successsful" : "Saving failed";
+            TempData["Message"] = message;
+            TempData["isSuccess"] = result > 0;
+
+            return Redirect("/Family");
         }
     }
 }
--- HPPADotNetCore.Mvc/Program.cs
+++ HPPADotNetCore.Mvc/Program.cs
@@ -8,10 +8,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
-},ServiceLifetime.Transient,
-ServiceLifetime.Transient
-);
+    string? connectionString = builder.Configuration.GetConnectionString("DbConnection");
+    options.UseSqlServer(connectionString);
+},
+ServiceLifetime.Transient,
+ServiceLifetime.Transient);
 
 var app = builder.Build();
 
