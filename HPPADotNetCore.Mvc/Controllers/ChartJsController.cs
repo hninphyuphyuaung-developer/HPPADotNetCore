0f89becdfc56a450b7db4ae84ab7baf6eaba8b8a
Hnin Phyu Phyu Aung <hninphyuphyuaung16@gmail.com>
2023-12-30 19:27:11 +0630

    [Complete] ChartJsMultiAxisLineChart

--- HPPADotNetCore.Mvc/Controllers/ChartJsController.cs
+++ HPPADotNetCore.Mvc/Controllers/ChartJsController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HPPADotNetCore.MvcApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HPPADotNetCore.MvcApp.Controllers
 {
@@ -13,6 +14,47 @@ namespace HPPADotNetCore.MvcApp.Controllers
         public IActionResult HorizontalBarChart()
         {
             return View();
+        }
+
+        public IActionResult MultiAxisLineChart()
+        {
+            ChartJsMultiAxisLineChartResponseModel model =new ChartJsMultiAxisLineChartResponseModel
+            {
+                DataCount = 7,
+                Labels = new List<string> {"January",
+                            "February",
+                            "March",
+                            "April",
+                            "May",
+                            "June",
+                            "July"},
+                Datasets = new List<ChartJsMultiAxisLineChartModel> 
+                { 
+                    new ChartJsMultiAxisLineChartModel 
+                    {
+                        label = "Dataset1",
+                        data = Enumerable.Range(1, 7).Select(x => GenerateData (1, 100)).ToList(),
+                        borderColor = "rgb(255, 99, 132)",
+                        backgroundColor = "rgb(255, 99, 132)",
+                        yAxisID = "y"
+                    },
+                    new ChartJsMultiAxisLineChartModel
+                    {
+                        label = "Dataset2",
+                        data = Enumerable.Range(1, 7).Select(x => GenerateData (1, 100)).ToList(),
+                        borderColor = "rgb(0, 191, 255)",
+                        backgroundColor = "rgb(0, 191, 255)",
+                        yAxisID = "y1"
+                    }
+                }
+            };
+            return View(model);
+        }
+
+        private int GenerateData(int from, int to)
+        {
+            Random random = new Random();
+            return random.Next(from, to);
         }
     }
 }
